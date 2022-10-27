cc362106e7fa6862b862684430d94a88391dad22
Jonas Jeronimo <jonas.jeronimo@live.com>
2022-10-27 16:29:17 -0300

    Requisito 58 e 59 concluido

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 export default function Profile() {
+  const [email, setEmail] = useState([]);
+
+  useEffect(() => {
+    const user = JSON.parse(localStorage.getItem('user'));
+    if (user) {
+      setEmail(user);
+    }
+  }, []);
   return (
     <div>
-      <p data-testid="profile-email">email</p>
+      <p data-testid="profile-email">{email.email}</p>
       <button
         type="button"
         data-testid="profile-done-btn"
